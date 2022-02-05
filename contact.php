c2e22c7194b1dbd068f28aa5a112d721be3eb442
Aleksandar Beronja <aleksandarberonja6@gmail.com>
2022-02-05 15:48:07 +0100

    add contact form with mail send

---
+++ contact.php
@@ -0,0 +1,33 @@
+<?php
+
+if (!$_POST) exit;
+
+if (!defined("PHP_EOL")) define("PHP_EOL", "\r\n");
+
+$name     = $_POST['name'];
+$email    = $_POST['email'];
+$subject  = $_POST['subject'];
+$message = $_POST['message'];
+
+if (get_magic_quotes_gpc()) {
+    $message = stripslashes($message);
+}
+
+$address = "bexaca@gmail.com";
+$e_subject = "$name Vam je poslao email.";
+$e_content = "$message" . PHP_EOL . PHP_EOL;
+$e_reply = "$name može biti kontaktiran putem emaila: $email.";
+
+$msg = wordwrap($e_content . $e_reply, 70);
+
+$headers = "From: $email" . PHP_EOL;
+$headers .= "Reply-To: $email" . PHP_EOL;
+$headers .= "MIME-Version: 1.0" . PHP_EOL;
+$headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
+$headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
+
+if (mail($address, $e_subject, $msg, $headers)) {
+    echo "$name";
+} else {
+    echo 'ERROR!';
+}
